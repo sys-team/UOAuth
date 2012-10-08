a52c156e143d5d7073eebeed72ef892a0edc1379
Alexander Levin <alevin.ru@gmail.com>
2012-10-08 23:20:28 +0400

    Косметика в handler.php

--- php/handler.php
+++ php/handler.php
@@ -1,23 +1,29 @@
 <?php
-    ////////////////////////
+
+    
+    define ('UOAUTH_SERVER', 'https://oldcat.unact.ru/iExp/UOAuth/');
+
+    
     function dumpNice ($array) {
         foreach ($array as $key=>$value)
             print $key .' = '. $value."<br/>";
     }    
-    ////////////////////////
+
+
     function asaPost ($service, $postDataArray) {
         
-        $ch = curl_init("https://hqvsrv58/iExp/UOAuth/".$service);
-    
+        $ch = curl_init( UOAUTH_SERVER . $service );
+        
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         
         foreach ($postDataArray as $key=>$value)
-            $data .= ($data?'&':'').$key .'='. $value;
+            $data .= ($data?'&':'').$key .'='. $value
+        ;
         
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    
+        
         $ret = curl_exec($ch);
         
         curl_close($ch);
@@ -25,7 +31,8 @@
         return $ret;
         
     }
-    ///////////////////////
+
+
     function asaReadResponse($response) {
         
         $xml = new DOMDocument();
@@ -38,37 +45,42 @@
         
         return $ret;
     }
-    ////////////////////////
+
+
     //print '<br/>$_SERVER:<br/><br/>';dumpNice ($_SERVER);
     //print '<br/>$_REQUEST:<br/><br/>';dumpNice ($_REQUEST);
     
     $parms = array();
 
     if ($_SERVER[TERM_PROGRAM]) {
+        
         print 'terminal';
-    }
-    else {
+        
+    } else {
+        
         $urlPathParts= array();
         $u = $_SERVER[SCRIPT_NAME];
         $u = substr($u, strpos($u,'/handler/'));
-
+        
         foreach (explode('/', $u) as $urlPart) 
-            array_push ($urlPathParts, $urlPart);
-    
+            array_push ($urlPathParts, $urlPart)
+        ;
+        
         if (count($urlPathParts) < 5) {
             print 'Wrong URL';
             return;
         }
         
         $service = $urlPathParts[2];
-            
-         switch ($service) {
+        
+        switch ($service) {
             case 'auth':
-            
+                
                 $parms['e_service'] = $urlPathParts[3];
                 $parms['client_id'] = $urlPathParts[4];
                 $parms['e_code'] = $_REQUEST['code'];
-                
+                break;
+            
             case 'token':
             
         }
@@ -79,7 +91,9 @@
     $asaResponseArray = asaReadResponse($asaResponse);
 
     switch ($service) {
+        
         case 'auth':
+            
             if(!$parms['client_id']) {
                 print 'Wrong client_id';
                 return;
@@ -101,9 +115,12 @@
             else{
                 echo ('Empty redirect URL');
             }
-            return;
+            
+        break;
         
         case 'token':
+            
+        break;
     }
     
     
