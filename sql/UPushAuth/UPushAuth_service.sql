dcc5bd69777e0cbd5bec23cb50343bb63d47512f
Alexey Panshin <alexey.i.panshin@gmail.com>
2013-01-15 13:29:50 +0400

    Folder and subproject UPushAuth removed

--- sql/UPushAuth/UPushAuth_service.sql
+++
@@ -1,7 +0,0 @@
-drop service UPushAuth
-;
-create service UPushAuth
-TYPE 'RAW' 
-AUTHORIZATION OFF USER "upa"
-url on
-as call util.xml_for_http(upa.UPushAuth(:url));
--- sql/UPushAuth/pushNotification.sql
+++
@@ -1,5 +0,0 @@
-create or replace function upa.pushNotification(url long varchar, token long varchar, "message" long varchar)
-returns long varchar
-url '!url'
-type 'http:get'
-;
