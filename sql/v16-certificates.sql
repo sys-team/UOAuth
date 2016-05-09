c08a566df48d3229c43c2888e9bdcee3739879eb
Alexander Levin <alevin.ru@gmail.com>
2016-05-09 10:49:41 +0300

    v16-certificates

---
+++ sql/v16-certificates.sql
@@ -0,0 +1,159 @@
+create certificate [*.sistemium.com] from
+'-----BEGIN CERTIFICATE-----
+MIIFWTCCBEGgAwIBAgIJAL0Mk8BG5YlqMA0GCSqGSIb3DQEBCwUAMIG0MQswCQYD
+VQQGEwJVUzEQMA4GA1UECBMHQXJpem9uYTETMBEGA1UEBxMKU2NvdHRzZGFsZTEa
+MBgGA1UEChMRR29EYWRkeS5jb20sIEluYy4xLTArBgNVBAsTJGh0dHA6Ly9jZXJ0
+cy5nb2RhZGR5LmNvbS9yZXBvc2l0b3J5LzEzMDEGA1UEAxMqR28gRGFkZHkgU2Vj
+dXJlIENlcnRpZmljYXRlIEF1dGhvcml0eSAtIEcyMB4XDTE1MDUyNTE3MTAzOFoX
+DTE2MDcxNjA1MTM1OFowYzELMAkGA1UEBhMCTFQxEDAOBgNVBAgTB1ZpbG5pdXMx
+EDAOBgNVBAcTB1ZpbG5pdXMxFjAUBgNVBAoTDVVBQiBTaXN0ZW1pdW0xGDAWBgNV
+BAMMDyouc2lzdGVtaXVtLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
+ggEBAJ7VJqb/N8Tj72TIgajif27jC5tGvHlPEuUZdMrEpfeIoVaNT/f6yGnw0P5X
+K4l0Td43n1/F2zqcuYGoajywO+9iEIr8GMMq0y3R2N3kx6OdNpCSZhZbV0Sw90tn
+KuOeAMzOcmcmm1aWJPKksmOMJfCM1G6F03WZPA6utars9C1RumP7k+6O12x4H5PA
+4tSMWhe4ZlHfotHVDv2qYwZfTvh6T9hhFMSmlQhM/fjaZD1dvPDbeoTStkn+qbsv
+9FQvsw+itOhpDtgtGzv5zBHPfSmoeTDmDH6Uydz0zh7TWqV/143ApsttWqFDIfVJ
+Y20+PgjM4y5FRbSAxbjzo1WKqvcCAwEAAaOCAbwwggG4MAwGA1UdEwEB/wQCMAAw
+KQYDVR0lBCIwIAYIKwYBBQUHAwEGCCsGAQUFBwMCBgpghkgBhvhNAQIDMA4GA1Ud
+DwEB/wQEAwIFoDA1BgNVHR8ELjAsMCqgKKAmhiRodHRwOi8vY3JsLmdvZGFkZHku
+Y29tL2dkaWcyczItMC5jcmwwUwYDVR0gBEwwSjBIBgtghkgBhv1tAQcXAjA5MDcG
+CCsGAQUFBwIBFitodHRwOi8vY2VydGlmaWNhdGVzLmdvZGFkZHkuY29tL3JlcG9z
+aXRvcnkvMHYGCCsGAQUFBwEBBGowaDAkBggrBgEFBQcwAYYYaHR0cDovL29jc3Au
+Z29kYWRkeS5jb20vMEAGCCsGAQUFBzAChjRodHRwOi8vY2VydGlmaWNhdGVzLmdv
+ZGFkZHkuY29tL3JlcG9zaXRvcnkvZ2RpZzIuY3J0MB8GA1UdIwQYMBaAFEDCvSeO
+zDSDMKIz1/tss/C0LIDOMCkGA1UdEQQiMCCCDyouc2lzdGVtaXVtLmNvbYINc2lz
+dGVtaXVtLmNvbTAdBgNVHQ4EFgQUiIhq6PvE8TcgfYzId0a1OVTuF/cwDQYJKoZI
+hvcNAQELBQADggEBAIrL9Ys3C5q/I3a7t+1+Pq6Dwz1KWjAe5JSetw34lx69g0Nb
+dCVnVdRJicWIYh4KbU5cMsacF7V5ulkt1Q94R9bOBvw/IzcLzksB8EvBgDpmjNE4
+8BPKXEhPaDzEGQalVjaQBvqaFMRKVZtru18msBP2L8+r5YoFW/yrmDbdGmhdqopt
+XkNwfq06TG/MYBx1m715lbWdSt6T8359p+CaSkPfIG+R32nBjZTkRs+mEMPk7gf2
+yg925oWq6C3twin59mNqiUpg5K2F7vRglArWdzRuthSz+3D3xGUeUviMzj6aXWQI
+k3oYDHSvMSrfOYEXR6JFftpoQgMMMr46Vmj/ohA=
+-----END CERTIFICATE-----
+-----BEGIN CERTIFICATE-----
+MIIE0DCCA7igAwIBAgIBBzANBgkqhkiG9w0BAQsFADCBgzELMAkGA1UEBhMCVVMx
+EDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNjb3R0c2RhbGUxGjAYBgNVBAoT
+EUdvRGFkZHkuY29tLCBJbmMuMTEwLwYDVQQDEyhHbyBEYWRkeSBSb290IENlcnRp
+ZmljYXRlIEF1dGhvcml0eSAtIEcyMB4XDTExMDUwMzA3MDAwMFoXDTMxMDUwMzA3
+MDAwMFowgbQxCzAJBgNVBAYTAlVTMRAwDgYDVQQIEwdBcml6b25hMRMwEQYDVQQH
+EwpTY290dHNkYWxlMRowGAYDVQQKExFHb0RhZGR5LmNvbSwgSW5jLjEtMCsGA1UE
+CxMkaHR0cDovL2NlcnRzLmdvZGFkZHkuY29tL3JlcG9zaXRvcnkvMTMwMQYDVQQD
+EypHbyBEYWRkeSBTZWN1cmUgQ2VydGlmaWNhdGUgQXV0aG9yaXR5IC0gRzIwggEi
+MA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC54MsQ1K92vdSTYuswZLiBCGzD
+BNliF44v/z5lz4/OYuY8UhzaFkVLVat4a2ODYpDOD2lsmcgaFItMzEUz6ojcnqOv
+K/6AYZ15V8TPLvQ/MDxdR/yaFrzDN5ZBUY4RS1T4KL7QjL7wMDge87Am+GZHY23e
+cSZHjzhHU9FGHbTj3ADqRay9vHHZqm8A29vNMDp5T19MR/gd71vCxJ1gO7GyQ5HY
+pDNO6rPWJ0+tJYqlxvTV0KaudAVkV4i1RFXULSo6Pvi4vekyCgKUZMQWOlDxSq7n
+eTOvDCAHf+jfBDnCaQJsY1L6d8EbyHSHyLmTGFBUNUtpTrw700kuH9zB0lL7AgMB
+AAGjggEaMIIBFjAPBgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBBjAdBgNV
+HQ4EFgQUQMK9J47MNIMwojPX+2yz8LQsgM4wHwYDVR0jBBgwFoAUOpqFBxBnKLbv
+9r0FQW4gwZTaD94wNAYIKwYBBQUHAQEEKDAmMCQGCCsGAQUFBzABhhhodHRwOi8v
+b2NzcC5nb2RhZGR5LmNvbS8wNQYDVR0fBC4wLDAqoCigJoYkaHR0cDovL2NybC5n
+b2RhZGR5LmNvbS9nZHJvb3QtZzIuY3JsMEYGA1UdIAQ/MD0wOwYEVR0gADAzMDEG
+CCsGAQUFBwIBFiVodHRwczovL2NlcnRzLmdvZGFkZHkuY29tL3JlcG9zaXRvcnkv
+MA0GCSqGSIb3DQEBCwUAA4IBAQAIfmyTEMg4uJapkEv/oV9PBO9sPpyIBslQj6Zz
+91cxG7685C/b+LrTW+C05+Z5Yg4MotdqY3MxtfWoSKQ7CC2iXZDXtHwlTxFWMMS2
+RJ17LJ3lXubvDGGqv+QqG+6EnriDfcFDzkSnE3ANkR/0yBOtg2DZ2HKocyQetawi
+DsoXiWJYRBuriSUBAA/NxBti21G00w9RKpv0vHP8ds42pM3Z2Czqrpv1KrKQ0U11
+GIo/ikGQI31bS/6kA1ibRrLDYGCD+H1QQc7CoZDDu+8CL9IVVO5EFdkKrqeKM+2x
+LXY2JtwE65/3YR8V3Idv7kaWKK2hJn0KCacuBKONvPi8BDAB
+-----END CERTIFICATE-----
+-----BEGIN CERTIFICATE-----
+MIIDxTCCAq2gAwIBAgIBADANBgkqhkiG9w0BAQsFADCBgzELMAkGA1UEBhMCVVMx
+EDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNjb3R0c2RhbGUxGjAYBgNVBAoT
+EUdvRGFkZHkuY29tLCBJbmMuMTEwLwYDVQQDEyhHbyBEYWRkeSBSb290IENlcnRp
+ZmljYXRlIEF1dGhvcml0eSAtIEcyMB4XDTA5MDkwMTAwMDAwMFoXDTM3MTIzMTIz
+NTk1OVowgYMxCzAJBgNVBAYTAlVTMRAwDgYDVQQIEwdBcml6b25hMRMwEQYDVQQH
+EwpTY290dHNkYWxlMRowGAYDVQQKExFHb0RhZGR5LmNvbSwgSW5jLjExMC8GA1UE
+AxMoR28gRGFkZHkgUm9vdCBDZXJ0aWZpY2F0ZSBBdXRob3JpdHkgLSBHMjCCASIw
+DQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAL9xYgjx+lk09xvJGKP3gElY6SKD
+E6bFIEMBO4Tx5oVJnyfq9oQbTqC023CYxzIBsQU+B07u9PpPL1kwIuerGVZr4oAH
+/PMWdYA5UXvl+TW2dE6pjYIT5LY/qQOD+qK+ihVqf94Lw7YZFAXK6sOoBJQ7Rnwy
+DfMAZiLIjWltNowRGLfTshxgtDj6AozO091GB94KPutdfMh8+7ArU6SSYmlRJQVh
+GkSBjCypQ5Yj36w6gZoOKcUcqeldHraenjAKOc7xiID7S13MMuyFYkMlNAJWJwGR
+tDtwKj9useiciAF9n9T521NtYJ2/LOdYq7hfRvzOxBsDPAnrSTFcaUaz4EcCAwEA
+AaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMCAQYwHQYDVR0OBBYE
+FDqahQcQZyi27/a9BUFuIMGU2g/eMA0GCSqGSIb3DQEBCwUAA4IBAQCZ21151fmX
+WWcDYfF+OwYxdS2hII5PZYe096acvNjpL9DbWu7PdIxztDhC2gV7+AJ1uP2lsdeu
+9tfeE8tTEH6KRtGX+rcuKxGrkLAngPnon1rpN5+r5N9ss4UXnT3ZJE95kTXWXwTr
+gIOrmgIttRD02JDHBHNA7XIloKmf7J6raBKZV8aPEjoJpL1E/QYVN8Gb5DKj7Tjo
+2GTzLH4U/ALqn83/B2gX2yKQOC16jdFU8WnjXzPKej17CuPKf1855eJ1usV2GDPO
+LPAvTK33sefOT6jEm0pUBsV/fdUID+Ic/n4XuKxe9tQWskMJDE32p2u0mYRlynqI
+4uJEvlz36hz1
+-----END CERTIFICATE-----'
+;
+
+create or replace function ua.systemProxyGet(
+    url long varchar
+) returns long varchar
+url '!url'
+type 'HTTP:POST'
+certificate 'cert_name=*.sistemium.com'
+;
+
+create or replace function ua.json2xml(@request long nvarchar)
+returns long nvarchar
+url 'https://api.sistemium.com/api/json2xml'
+type 'HTTP:POST:text/json'
+header 'Content-type:text/json'
+certificate 'cert_name=*.sistemium.com'
+;
+
+
+create or replace function mailru.processAuthCode(
+    url long varchar,
+    _address long varchar,
+    client_id long varchar,
+    client_secret long varchar,
+    grant_type long varchar,
+    code long varchar,
+    redirect_uri long varchar
+)
+returns long varchar
+url '!url'
+type 'HTTP:POST'
+certificate 'cert_name=*.sistemium.com'
+;
+
+
+create or replace function google.processAuthCode(
+    url varchar(1024),
+    code varchar(1024),
+    client_id varchar(1024),
+    client_secret varchar(1024),
+    redirect_uri varchar(1024) default 'urn:ietf:wg:oauth:2.0:oob',
+    grant_type varchar(1024) default 'authorization_code'
+)
+returns long varchar
+url '!url'
+type 'HTTP:POST'
+certificate 'cert_name=*.sistemium.com'
+;
+
+create or replace function vk.processAuthCode (
+    url varchar(1024),
+    code varchar(1024),
+    client_id varchar(1024),
+    client_secret varchar(1024),
+    redirect_uri varchar(1024)
+) returns long varchar
+url '!url'
+type 'HTTP:POST'
+certificate 'cert_name=*.sistemium.com'
+;
+
+
+create or replace function odks.processAuthCode(
+    url long varchar,
+    _address long varchar,
+    code long varchar,
+    redirect_uri long varchar,
+    client_id long varchar,
+    client_secret long varchar,
+    grant_type long varchar default 'authorization_code'
+)
+returns long varchar
+url '!url'
+type 'HTTP:POST'
+certificate 'cert_name=*.sistemium.com'
+;
